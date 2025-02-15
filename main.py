fe3219f4b841a6153fb20130999e0b5d864113d7
SajaDevil <ssacraft3@gmail.com>
2025-02-15 23:19:20 +0300

    now the numbers of chapters are better

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 # Replace with the actual audiobook page URL
-book_url = "https://tokybook.com/providence-the-beginning-after-the-end-book-11"
+book_url = "https://tokybook.com/divergence"
 
 # Fetch the audiobook page
 response = requests.get(book_url)
@@ -31,7 +31,7 @@ for script in soup.find_all("script"):
                     
 # enumerate returns the value and its index (index start at 1 in this case)                    
 for i,link in enumerate(chapter_links,start=1): 
-    file_name = os.path.join(book_title,f"chapter {i}.mp3")
+    file_name = os.path.join(book_title,f"chapter {i:02}.mp3")
     chap_response = requests.get(link,stream=True)
     chap_size = int(chap_response.headers.get("content-length",0))
     
@@ -40,7 +40,7 @@ for i,link in enumerate(chapter_links,start=1):
         # and creates a progress bar with correct units
         with open(file_name,"wb") as f , tqdm(total=chap_size,unit="B"
                                               ,unit_scale=True,unit_divisor=1024
-                                              ,desc=f"downloading chapter {i}.mp3:") as progress:
+                                              ,desc=f"downloading chapter {i:02}.mp3:") as progress:
             # reads the mp3 file from the link in a 1 MB chunks
             for chunk in chap_response.iter_content(chunk_size=2**20):
                 f.write(chunk)
